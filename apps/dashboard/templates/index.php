ab6116d4a31802b5938351eff3ae1b5fa0b87a35
Julien Veyssier <eneiluj@posteo.net>
2020-08-06 10:43:26 +0200

    new weather status used in dashboard
    Signed-off-by: Julien Veyssier <eneiluj@posteo.net>

--- apps/dashboard/templates/index.php
+++ apps/dashboard/templates/index.php
@@ -1,4 +1,7 @@
 <?php
 	\OCP\Util::addScript('dashboard', 'dashboard');
+	if (\OC::$server->getAppManager()->isEnabledForUser('weather_status')) {
+		\OCP\Util::addScript('weather_status', 'weather-status');
+	}
 ?>
 <div id="app-content-vue"></div>
